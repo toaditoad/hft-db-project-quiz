f8fe93951d0e430ee5d94c3b1e98e89ad730e72a
toaditoad <communitiestk@me.com>
2016-06-08 21:50:43 +0200

    cleaned code

--- src/org/hft/databases/project/controller/QuizController.java
+++ src/org/hft/databases/project/controller/QuizController.java
@@ -5,7 +5,6 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
